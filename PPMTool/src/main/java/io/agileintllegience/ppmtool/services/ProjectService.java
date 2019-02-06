16786be69535eafc8818bfd39e60d7b21253252a
muslumgezgin <muslum_gezgin>
2019-02-06 22:17:16 +0100

    find Project by Identifier

--- PPMTool/src/main/java/io/agileintllegience/ppmtool/services/ProjectService.java
+++ PPMTool/src/main/java/io/agileintllegience/ppmtool/services/ProjectService.java
@@ -21,4 +21,17 @@ public class ProjectService {
             throw  new ProjectIdException("Project ID'"+project.getProjectIdentifier().toUpperCase()+"'+already exist");
         }
     }
+
+    public Project findProjectByIdentifier(String projectId){
+
+        Project project = projectRepository.findByProjectIdentifier(projectId.toUpperCase());
+
+        if(project == null){
+            throw new ProjectIdException("Project ID '"+projectId.toUpperCase()+"' does not exist");
+        }
+        return project;
+    }
+
+
+
 }
