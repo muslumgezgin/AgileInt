d088586427fe4634b1ea126cea5495b38cda6d3c
muslumgezgin <muslum_gezgin>
2019-02-08 15:37:55 +0100

    added Project header componenets on the React App

--- PPMTool-react-client/src/components/Dashboard.js
+++ PPMTool-react-client/src/components/Dashboard.js
@@ -1,11 +1,17 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import ProjectItem from "./Project/ProjectItem";
+import Header from "./Layout/Header";
 
-export default class Dashboard extends Component {
+class Dashboard extends Component {
   render() {
     return (
       <div>
-        
+        <Header />
+        <h1>Welcome to Dashboard</h1>
+        <ProjectItem />
       </div>
-    )
+    );
   }
 }
+
+export default Dashboard;
---
+++ PPMTool-react-client/src/components/Project/ProjectItem.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+ 
+class ProjectItem extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Project Items</h1>
+      </div>
+    )
+  }
+}
+
+export default ProjectItem;
